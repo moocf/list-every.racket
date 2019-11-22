40a50cd5a435256f915edc99c15ee815e43d5f8b
wolfram77 <wolfram77@gmail.com>
2019-11-22 16:34:15 +0530

    functional programming on lists

---
+++ main.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+; (every? pred lst): F X L -> B
+; returns #f if any element of lst fails to satisfy pred, #t otherwise
+; : size(lst)=0      -> #t
+; : (pred lst[0])=#t -> (every pred lst[1..end])
+; : else             -> #f
+(define every?
+  (lambda (pred lst)
+    (if (null? lst)
+        #t
+        (if (pred (car lst))
+            (every? pred (cdr lst))
+            #f))))
