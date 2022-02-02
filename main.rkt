227aeeec5944248d4a91f57263032be122f4d480
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:51:12 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,14 +1,14 @@
 #lang racket
 
-; (every? pred lst): F X L -> B
+; (list.every pred lst): F X L -> B
 ; returns #f if any element of lst fails to satisfy pred, #t otherwise
 ; : size(lst)=0      -> #t
 ; : (pred lst[0])=#t -> (every pred lst[1..end])
 ; : else             -> #f
-(define every?
+(define list.every
   (lambda (pred lst)
     (if (null? lst)
         #t
         (if (pred (car lst))
-            (every? pred (cdr lst))
+            (list.every pred (cdr lst))
             #f))))
